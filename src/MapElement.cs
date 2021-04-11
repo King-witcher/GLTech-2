7e6192b6877129e473e568534221427ef5f61243
Giuseppe Alma <giuseppelanna2000@users.noreply.github.com>
2021-04-11 18:16:58 -0300

    MapElement class added.

---
+++ src/MapElement.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace GLTech2
+{
+    public abstract unsafe class MapElement : IDisposable
+    {
+        internal void* unmanaged;
+        private bool isBound;
+        private bool disposed;
+
+        public bool IsBound
+        {
+            get => isBound;
+            internal set => isBound = value;
+        }
+
+        internal void Bind(void* newAddress)
+        {
+            if (isBound)
+                throw new InvalidOperationException("MapElement already bound.");
+            if (disposed)
+                throw new ObjectDisposedException("MapElement");;
+            unmanaged = newAddress;
+            isBound = true;
+        }
+
+        public virtual void Dispose()
+        {
+            if (disposed)
+                return;
+            Marshal.FreeHGlobal((IntPtr)unmanaged);
+            disposed = true;
+        }
+    }
+}
