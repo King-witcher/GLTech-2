8d848a9cdb88be06936ef6df3880f0fcedfae309
King-witcher <giuseppe.2000@hotmail.com>
2021-04-30 12:05:21 -0300

    TestEffect class

---
+++ src/PostProcessing/TestEffect.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GLTech2.PostProcessing
+{
+    internal class TestEffect : Effect
+    {
+        internal override void Process(PixelBuffer target)
+        {
+        }
+    }
+}
