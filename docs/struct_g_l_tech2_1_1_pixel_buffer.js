27554ae5c4d49d923ae8c5a6d12aba17a2186915
King-witcher <giuseppe.2000@hotmail.com>
2021-05-03 22:56:09 -0300

    dox update

--- docs/struct_g_l_tech2_1_1_pixel_buffer.js
+++
@@ -1,5 +0,0 @@
-var struct_g_l_tech2_1_1_pixel_buffer =
-[
-    [ "Dispose", "struct_g_l_tech2_1_1_pixel_buffer.html#aa0da7bf121a380b3061a24d0542c0f77", null ],
-    [ "this[int x, int y]", "struct_g_l_tech2_1_1_pixel_buffer.html#ae43a9d8f0ec4039d7051099965425c7c", null ]
-];
